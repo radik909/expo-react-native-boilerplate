460f86dbb5595994a67121a08665f29fc67a9786
radik909 <rahuldinesh1991@gmail.com>
2018-06-03 23:43:47 +0530

    Login & Register buttons

--- src/screens/Landing/index.js
+++ src/screens/Landing/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import Expo from 'expo';
 
 const styles = StyleSheet.create({
   container: {
@@ -13,5 +15,39 @@ const styles = StyleSheet.create({
 export default () => (
   <View style={styles.container}>
     <Text>A unique place to find your perfect life partner!</Text>
+    <Button
+      title="LOGIN"
+      titleStyle={{ fontSize: 16 }}
+      raised
+      buttonStyle={{
+        backgroundColor: '#000000',
+        borderRadius: 50,
+      }}
+      ViewComponent={Expo.LinearGradient}
+      linearGradientProps={{
+        colors: ['#FF9800', '#F44336'],
+        start: [1, 0],
+        end: [0.2, 0],
+      }}
+      containerStyle={{ width: '100%', padding: 15 }}
+      onPress={() => console.log('Pressed')}
+    />
+    <Button
+      title="REGISTER"
+      titleStyle={{ fontSize: 16 }}
+      raised
+      buttonStyle={{
+        backgroundColor: '#000000',
+        borderRadius: 50,
+      }}
+      ViewComponent={Expo.LinearGradient}
+      linearGradientProps={{
+        colors: ['#FF9800', '#F44336'],
+        start: [1, 0],
+        end: [0.2, 0],
+      }}
+      containerStyle={{ width: '100%', padding: 15 }}
+      onPress={() => console.log('Pressed')}
+    />
   </View>
 );

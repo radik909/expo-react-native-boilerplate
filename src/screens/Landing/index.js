ef00dc37b5e3bfd91edd71307cbae2ad14103317
radik909 <rahuldinesh1991@gmail.com>
2018-06-03 23:53:25 +0530

    Android - Status Bar configuration

--- src/screens/Landing/index.js
+++ src/screens/Landing/index.js
@@ -9,6 +9,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: Expo.Constants.statusBarHeight,
   },
 });
 
